8935a56cb1555952f26aa79760a6aa04205b6093
Williams Barriquero <williamsgomess@outlook.com>
2023-11-29 15:46:14 -0300

    Novo Roteador

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,15 @@
 package routes
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/williamsbarriquero/go-rest-api/controllers"
 	"log"
 	"net/http"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
