18838ff8cea406c55d11cdc8287908140a18a694
Williams Barriquero <williamsgomess@outlook.com>
2023-11-29 14:44:37 -0300

    01. Json, Request, Response e Go
    Modularizando o código

--- main.go
+++ main.go
@@ -2,20 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"github.com/williamsbarriquero/go-rest-api/routes"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
 	fmt.Println("Iniciando servidor Rest com Go!")
-	HandleRequest()
+	routes.HandleRequest()
 }
